fa4aa075626ac9b73a456372ff3811249e325f85
GaleanoSantiago <104083617+GaleanoSantiago@users.noreply.github.com>
2022-12-13 20:53:04 -0300

    nuevas imagenes optimizadas para los proyectos y nuevo formato de seccion proyectos

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ const itemProjs = document.getElementById("itemProjects");
 const itemContact = document.getElementById("itemContacto");
 const itemsMenu = document.querySelectorAll(".nav-link");
 
+/*---------------Modal---------------*/
+const modal = document.querySelectorAll(".mdl");
+// console.log(modal);
+
 /*---------------Footer---------------*/
 const contenedorIcons = document.querySelectorAll(".icon-container");
  
@@ -116,6 +120,86 @@ contenedorIcons.forEach(icon => {
     });
 });
 
+/*--------------Prueba generar carousel-item dinamicamente--------------*/
+
+modal.forEach(mdl => {
+    const modalBody = document.querySelector(".modal-body");
+    const modalHeader = document.querySelector(".modal-header");
+    let valueMdl = mdl.value;
+    let infoModales = [
+    {
+        titulo: `CoinCoders`,
+        descripcion:`Sitio web realizado para una empresa 
+        de desarrollo de software. Totalmente responsive 
+        y con un formulario de contacto funcional. 
+        Desarrollada con una mezcla de css, bootstrap y javascript.`,
+        lenguajes: `HTML - CSS - JS`
+
+    },
+    {
+        titulo:`Portafolio`,
+        descripcion:`Incluyo este mismo portafolio como un proyecto 
+        en mi galería debido al trabajo y esfuerzo que me tomó 
+        desarrollarlo hasta el nivel en el que se encuentra ahora. 
+        Este portafolio es completamente responsive y la mayoría de efectos 
+        y animaciones fueron creados por mí.`,
+        lenguajes: `HTML - CSS - JS`
+
+    },
+    {
+        titulo:`Calculadora de Dias`,
+        descripcion:`Sitio web interactivo, ingrese una fecha 
+        cualquiera y el programa mostrará la cantidad de días 
+        exactos que hay entre esa fecha y el presente. 
+        Los procesos llevados a cabo por el programa son realizados en JavaScript.`,
+        lenguajes: `HTML - CSS - JS`
+
+    },
+    {
+        titulo:`Random User Generator`,
+        descripcion:`
+        Generador de usuario aleatorio creado con javascript y 
+        mediante la utilizacion de una API para la informacion 
+        de los usuarios. El color del background tambien cambia con cada usuario.`,
+        lenguajes: `HTML - CSS - JS`
+    },
+    {
+        titulo:`Sistema de Venta`,
+        descripcion:`
+        CRUD desarrollado para un negocio de mi ciudad, 
+        el proyecto que se encuentra en el hosting es una versión 
+        sin los detalles del negocio en cuestión, y con algunas diferencias leves.`,
+        lenguajes: `HTML - CSS - JS - PHP - SQL`
+    },
+    {
+        titulo:`Sistema de Venta y Control de Stock`,
+        descripcion:`
+        CRUD de venta y control de stock para pymes. 
+        Una versión mucho más compleja y extensa que 
+        el sistema visto anteriormente. <br>
+        Usuarios: <br>
+        -Administrador: admin - admin <br>
+        -Cajero: Xpress - 12345        
+        `,
+        lenguajes: `HTML - CSS - JS - PHP - SQL`
+    }
+    ]
+    mdl.addEventListener("click", ()=>{
+        // console.log(valueMdl);
+        // console.log(infoModales[valueMdl].titulo);
+        // console.log(infoModales[valueMdl].descripcion);
+        
+        modalHeader.innerHTML = `
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">${infoModales[valueMdl].titulo}</h1>
+        <button type="button" class="bg-light btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        `;
+        modalBody.innerHTML = `
+        <p class="text-gray">${infoModales[valueMdl].descripcion}</p>
+        <p>Lenguajes Utilizados: <span class="text-gray">${infoModales[valueMdl].lenguajes}</span></p>
+        `;
+
+    });
+});
 
 const activarItemsMenu = (tamanoDePantalla)=>{
     const inicio = document.getElementById("inicio");
